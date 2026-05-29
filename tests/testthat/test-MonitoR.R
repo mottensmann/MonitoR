efb8f8ccd296f3912520564e6941a0137ad8306c
Meinolf Ottensmann <mottensmann@users.noreply.github.com>
2026-05-29 21:02:56 +0200

    updated tests

--- tests/testthat/test-MonitoR.R
+++ tests/testthat/test-MonitoR.R
@@ -1,5 +1,22 @@
 ## read_birdnet_slist ----------------------------------------
-test_that("slist is up to date", {
+test_that("slist", {
   expect_equal(as.data.frame(read_birdnet_slist(.cached = F)), as.data.frame(read_birdnet_slist(.cached = T)))
 })
 
+
+## birdNET_process -------------------------------------------
+predictions <- birdNET_process(
+  audio = system.file("extdata", "20211220_064253.wav", package = "MonitoR"))
+test_that("birdNET_process", {
+  expect_equal(predictions[["species_name"]][1], 'Glaucidium passerinum_Sperlingskauz')
+})
+
+## birdNET_process_batch -------------------------------------
+predictions <- birdNET_process_batch(
+  wave_files = system.file("extdata", "20211220_064253.wav", package = "MonitoR"))
+predictions <- as.data.frame(predictions)
+test_that("birdNET_process_batch", {
+  expect_equal(predictions[["species_name"]][1], 'Glaucidium passerinum_Sperlingskauz')
+})
+
+unlink(system.file("extdata", "20211220_064253.BirdNET.results.txt", package = "MonitoR"))
